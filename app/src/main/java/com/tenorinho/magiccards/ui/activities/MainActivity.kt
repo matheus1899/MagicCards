876197ee18f1dc65bb4875206cf6911b115fc2bc
Matheus Tenorio <matheus.santos1899@gmail.com>
2021-03-18 11:15:44 -0300

    Atualizado o layout da MainActivity; Criado a SplashActivity e seu respectivo layout; Adicionado os drawables necessários;

--- app/src/main/java/com/tenorinho/magiccards/ui/activities/MainActivity.kt
+++ app/src/main/java/com/tenorinho/magiccards/ui/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.tenorinho.magiccards.ui.activities
 
 import android.os.Bundle
+import android.view.Menu
 import com.tenorinho.magiccards.R
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.marginBottom
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val t = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(t)
     }
 }
---
+++ app/src/main/java/com/tenorinho/magiccards/ui/activities/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.tenorinho.magiccards.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.tenorinho.magiccards.R
+
+class SplashActivity : AppCompatActivity(){
+    private val KEY = "first"
+    private var first = true
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+    override fun onResume() {
+        super.onResume()
+        if(first){
+            Handler().postDelayed({ navigateToMainActivity() }, 2000)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY, false)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState.containsKey(KEY)){
+            first = savedInstanceState.getBoolean(KEY)
+        }
+    }
+    private fun navigateToMainActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
